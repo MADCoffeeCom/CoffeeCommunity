97da922f64ccddbf7bdb05c0504e94cc3abc54e4
KyangTan <kwanyang29@gmail.com>
2023-01-05 14:24:30 +0800

    maps activity partially done

--- app/src/main/java/com/example/coffeecom/Providers/Baristas.java
+++ app/src/main/java/com/example/coffeecom/Providers/Baristas.java
@@ -2,7 +2,9 @@ package com.example.coffeecom.Providers;
 
 import android.util.Log;
 
+import com.example.coffeecom.Provider;
 import com.example.coffeecom.model.BaristaModel;
+import com.vishnusivadas.advanced_httpurlconnection.FetchData;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import java.lang.reflect.Array;
@@ -35,15 +37,39 @@ public void fetchAndSetBaristas(){
 //    data[0] = username.getText().toString();
 //    data[1] = passwordTextView.getText().toString();
 
-    PutData putData = new PutData("http://192.168.56.1/CoffeeCommunity/barista.php", "POST", field, data);
+//    PutData putData = new PutData("http://192.168.56.1/CoffeeCommunity/barista.php", "POST", field, data);
 
-    if (putData.startPut()){
-        if (putData.onComplete()){
-            String result = putData.getResult();
-            Log.d("bruhb",result);
+    FetchData fetchData = new FetchData("http://192.168.56.1/CoffeeCommunityPHP/barista.php");
+    if (fetchData.startFetch()) {
+        if (fetchData.onComplete()) {
+            String result = fetchData.getResult();
+            //End ProgressBar (Set visibility to GONE)
+            String[] resultSplitted = result.split("split");
+            for (String str: resultSplitted) {
+//                            Log.i(str, "Printing all user...");
 
+                String[] baristaDetails = str.split(" - ");
+                BaristaModel barista = new BaristaModel(baristaDetails[0],baristaDetails[1],baristaDetails[2],baristaDetails[3],baristaDetails[4],baristaDetails[5],baristaDetails[6],baristaDetails[7],baristaDetails[8],baristaDetails[9],baristaDetails[10]);
+                String userId = profileDetails[0];
+                if(userId.equals(Provider.getUser().getUserId())){
+                    String picUrl = profileDetails[1];
+                    String userName = profileDetails[2];
+                    String email = profileDetails[3];
+                    String streetNo = profileDetails[4];
+                    String taman = profileDetails[5];
+                    int postCode = Integer.parseInt(profileDetails[6]);
+                    String state = profileDetails[7];
+
+                    String walletId = profileDetails[8];
+                    double walletBalance = Double.valueOf(profileDetails[9]);
+                    String walletPin = profileDetails[10];
+
+                    Provider.getUser().setUserDetails(picUrl, userId, userName, email, "user", streetNo, taman, postCode, state, walletId, walletPin, walletBalance);
+                    Log.i(Provider.getUser().getUserName(), "User Updated in Provider");
+
+                }
+            }
         }
-
     }
 
 
