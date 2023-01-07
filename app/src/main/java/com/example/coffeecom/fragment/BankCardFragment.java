e47575e25f49911424a9c8b623aa848d517f5be8
Cattlepillow <u2102772@siswa.um.edu.my>
2023-01-08 01:59:42 +0800

    just set up linearlayout manager,me buhui set adapter...

--- app/src/main/java/com/example/coffeecom/fragment/BankCardFragment.java
+++ app/src/main/java/com/example/coffeecom/fragment/BankCardFragment.java
@@ -1,24 +1,47 @@
 package com.example.coffeecom.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.coffeecom.R;
+import com.example.coffeecom.adapter.BankCardAdapter;
+import com.example.coffeecom.adapter.LearnArticleAdapter;
 
 
 public class BankCardFragment extends Fragment {
+    private RecyclerView recyclerViewBankCardCardList;
+    private RecyclerView.Adapter bankCardAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.activity_bank_card,container,false);
 
+        initialiseID(view);
+    //    recyclerViewBankCardCard();
 
         // Inflate the layout for this fragment
         return view;
     }
+
+    private void initialiseID(View view) {
+        recyclerViewBankCardCardList = view.findViewById(R.id.bankCardCardRecycleView);
+    }
+
+    private void recyclerViewBankCardCard() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewBankCardCardList.setLayoutManager(linearLayoutManager);
+
+    //    bankCardAdapter = new BankCardAdapter();
+        recyclerViewBankCardCardList.setAdapter(bankCardAdapter);
+    }
+
 }
